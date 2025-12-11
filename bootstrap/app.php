5b4441393cf6ecd5891df261a807d37b072b1c79
batey96Trial <darius38130@gmail.com>
2025-12-11 11:55:44 +0100

    feat: use a single renderer for exceptions handling - Handle ModelNotFoundException / NotFoundHttpException → 404 with "Resource Not Found" - Handle AuthorizationException / AccessDeniedHttpException → 403 with policy message or "Forbidden"

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use function PHPUnit\Framework\isInstanceOf;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,25 +20,24 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->throttleWithRedis();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->render(function (ModelNotFoundException $e ) {
-            return response()->json(
-                [
+        $exceptions->render(function (\Throwable $e) {
+            if ($e instanceof ModelNotFoundException || $e instanceof NotFoundHttpException) {
+                return response()->json(
+                    [
+                        'status' => 'error',
+                        'message' => 'Resource Not Found'
+                    ],
+                    404
+                );
+            }
+
+            if ($e instanceof AuthorizationException || $e instanceof AccessDeniedHttpException) {
+                return response()->json([
                     'status' => 'error',
-                    'message' => 'User Not Found'
-                ],
-                404
-            );
+                    'message' => $e->getMessage() ?: 'Forbidden'
+                ], 403);
+            }
+
         });
-        $exceptions->render(function (AccessDeniedHttpException $e, $request) {
-        return response()->json([
-            'status'  => 'error',
-            'message' => $e->getMessage() ?: 'Forbidden'
-        ], 403);
-    });
-    $exceptions->render(function (AuthorizationException $e, $request) {
-        return response()->json([
-            'status'  => 'error',
-            'message' => $e->getMessage() ?: 'Forbidden'
-        ], 403);
-    });
+
     })->create();
